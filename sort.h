89be371c528d28b5086820cb79489b92c5008250
0xTariq-dev <tariqmohsen222@gmail.com>
2023-10-21 06:38:05 -0700

    feat: Add header file and printing functions

---
+++ sort.h
@@ -0,0 +1,40 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/types.h>
+
+/*
+ * file: sort.h
+ * author: 0xTariq-dev, Ehab Elsayed
+ * describtion: Header file contains declarations for functions
+ *		used in the Sorting Algorithms project.
+ */
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Functions Prototypes */
+
+/* Printing Functions */
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+/* Sorting Algorithms Prototypes */
+void bubble_sort(int *array, size_t size);
+void swap(int *x, int *y);
+
+#endif
